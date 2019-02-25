162be6bd35867ed9314fe219f0658ac700670882
TyrionJYQ <1003518719@qq.com>
2019-02-25 13:49:59 +0800

    优化http.js，减少参数

--- public/common/http.js
+++ public/common/http.js
@@ -1,24 +1,40 @@
 const Http = {}
 Http.config = {
-  methods: {
-    POST: 'POST',
-    DELETE: 'DELETE',
-    PUT: 'PUT',
-    GET: 'GET'
-  },
-  dataType: {
-    default: 'json'
+  // methods: {
+  //   POST: 'POST',
+  //   DELETE: 'DELETE',
+  //   PUT: 'PUT',
+  //   GET: 'GET'
+  // },
+  // dataType: {
+  //   default: 'json'
+  // },
+  defaultConfig: {
+    dataType: 'json',
+    methods: {
+      POST: 'POST',
+      DELETE: 'DELETE',
+      PUT: 'PUT',
+      GET: 'GET'
+    },
   },
   urls: {
     checkUsername: '/user/checkUsername'
   }
 }
-Http.request = function (url, data, reqMethod, dataType, success, failed) {
+Http.request = function (requsetData,success, failed) {
+  const {defaultConfig} = Http.config;
+  config = {
+    type: requsetData.type || defaultConfig.POST,
+    url:  defaultConfig.urls[requestData.url],
+    data: requestData.data,
+    dataType: requsetData.dataType || defaultConfig.dataType 
+  }
   $.ajax({
-    type: reqMethod,
-    url: url,
-    data: data,
-    dataType: dataType,
+    type: config.type,
+    url: config.url,
+    data: config.data,
+    dataType: config.dataType,
     success: success,
     failed: function(e) {
       console.log(e)
